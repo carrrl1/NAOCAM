c2beca0b713f37a5a3097213e71d0bfe89dbb036
Carlos Eduardo Solano <charlie@Charlies-MacBook-Air.local>
2017-05-03 13:51:41 -0600

    Fixed syntax error.

--- src/naocam.h
+++ src/naocam.h
@@ -61,7 +61,7 @@ class NaoCam : public AL::ALModule
     void move_to(float theta);
     void trackingObject();
     void featureTracking(Mat img_1, Mat img_2, vector<Point2f>& points1, vector<Point2f>& points2, vector<uchar>& status);
-    void featureDetection(Mat img_1, vector<Point2f>& points1)
+    void featureDetection(Mat img_1, vector<Point2f>& points1);
 
 ///Define private variables.
   private:
